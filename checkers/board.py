548ee56135d44bc4498121c662cc4ec28e2f84b3
Artis Cevers <ceversartis@gmail.com>
2023-03-02 00:39:29 +0200

    Added pieces to the table

--- checkers/board.py
+++ checkers/board.py
@@ -1,5 +1,6 @@
 import pygame
-from checkers.constants import BLACK, RED, ROWS, SQUARE_SIZE
+from .constants import BLACK, RED, ROWS, COLS, SQUARE_SIZE, WHITE
+from .piece import Piece
 
 class Board:
     def __init__(self):
@@ -7,6 +8,7 @@ class Board:
         self.selected_piece = None
         self.red_left = self.white_left = 12
         self.red_kings = self.white = 0
+        self.create_board()
     
     def draw_squares(self, win):
         win.fill(BLACK)
@@ -14,5 +16,39 @@ class Board:
             for col in range(row % 2, ROWS, 2):
                 pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)) 
     
+    def move(self, piece, row, col):
+        self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
+        piece.move(row, col)
+
+        if row == ROWS or row == 0:
+            piece.make_king()
+            if piece.color == WHITE:
+                self.white_kings += 1
+            else:
+                self.red_kings += 1
+
+    def get_piece(self, row, col):
+        return self.board[row][col]
+
     def create_board(self):
-        pass
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row + 1) % 2):
+                    if row < 3:
+                        self.board[row].append(Piece(row, col, WHITE))
+                    elif row > 4:
+                        self.board[row].append(Piece(row, col, RED))
+                    else: 
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
+
+    def draw(self, win):
+        self.draw_squares(win)  
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != 0:
+                    piece.draw(win)
+                        
